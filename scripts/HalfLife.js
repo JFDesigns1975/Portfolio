eaf3860a49c15613db1e92626c805c3bddc59b8d
JFDesigns1975 <55851908+JFDesigns1975@users.noreply.github.com>
2022-01-03 22:00:41 -0500

    Latest update, bug fixes for mobile viewing.

---
+++ scripts/HalfLife.js
@@ -0,0 +1,27 @@
+const video = document.getElementById("videoBackground");
+const sound = document.getElementById("sound");
+const links = document.querySelectorAll("ul a");
+const audioClick = document.getElementById("audioClick");
+const audioHover = document.getElementById("audioHover");
+
+sound.addEventListener("click", () => {
+    sound.classList.toggle("fa-volume-up");
+    video.muted = video.muted === false;
+    clickSound();
+});
+
+sound.addEventListener("mouseenter", hoverSound);
+
+for (let i = 0; i < links.length; i++) {
+
+    links[i].addEventListener("click", clickSound);
+    links[i].addEventListener("mouseenter", hoverSound);
+}
+
+function clickSound() {
+    audioClick.play();
+}
+
+function hoverSound(){
+    audioHover.play();
+}
